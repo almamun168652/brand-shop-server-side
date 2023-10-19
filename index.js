345211f7e48ea614d5daeb22b0a651519912408e
Al Mamun <wdmamun75@gmail.com>
2023-10-19 21:04:55 +0600

    added .env file and git ignore

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const app = express();
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -9,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://brandShop:8s9mQfjTEOjdqvIm@cluster0.ywavnlw.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ywavnlw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
